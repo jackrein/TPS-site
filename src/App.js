a725b88ff486ff2bed355cbdfdc324975ba86f6d
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-09-03 17:43:26 -0600

    Create site, add TPS crest and OpenDyslexic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './TPS_Crest.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the home of Theta Pi Sigma at the University of Colorado!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about us
         </a>
       </header>
     </div>
