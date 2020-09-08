650969557e5b69d7905026410d70616974934046
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-09-08 16:18:05 -0600

    Import react-bootstrap, add Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './TPS_Crest.svg';
 import './App.css';
+import { Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -16,6 +18,24 @@ function App() {
           <em>established 2018</em>
         </span>
       </header>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src="/TPS_Crest.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Theta Pi Sigma Crest"
+          />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#events">Events</Nav.Link>
+          <Nav.Link href="#gallery">Gallery</Nav.Link>
+          <Nav.Link href="#links">Links</Nav.Link>
+          <Nav.Link href="#contact">Contact Us</Nav.Link>
+        </Nav>
+      </Navbar>
       <div className="App-main">
         <p id="intro">
           Welcome to the home of <b>Theta Pi Sigma</b> at the University of Colorado!
